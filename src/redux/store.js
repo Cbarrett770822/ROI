1d227bc6e810c50d2ea0808909b544a723d766ee
WMS Tutorial User <wms.tutorial@example.com>
2025-07-05 01:07:51 +0800

    Fix authReducer import path in Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import companiesReducer from './slices/companiesSlice';
 import questionnaireReducer from './slices/questionnaireSlice';
 import uiReducer from './slices/uiSlice';
+import authReducer from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
     companies: companiesReducer,
     questionnaire: questionnaireReducer,
     ui: uiReducer,
+    auth: authReducer,
   },
 });
